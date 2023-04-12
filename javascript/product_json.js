b69b189d4a02b2e8ef7d82bf648cbe1765fd54e5
Ochirsukh0819 <oojoo20020819@gmail.com>
2023-04-12 20:49:17 +0800

    json bin deeree bairshuulav

--- javascript/product_json.js
+++ javascript/product_json.js
@@ -1,6 +1,6 @@
 import defaultImport from "../modules/product.js";
 
-const product = new defaultImport('./json/product.json');
+const product = new defaultImport('https://api.jsonbin.io/v3/b/6436a852ebd26539d0a99169');
 
 product.download();
 
