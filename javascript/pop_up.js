9d947cc00235e3ec958166b5b9f08b73c119afe7
Temuulen918 <112910669+Temuulen918@users.noreply.github.com>
2023-05-03 23:34:24 +0800

    pop up hiiv

---
+++ javascript/pop_up.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  const previewContainer = document.querySelector('.products-preview');
+  let previewBox = previewContainer.querySelectorAll('.preview');
+
+  document.querySelectorAll('#food .myproduct').forEach(myproduct => {
+    myproduct.onclick = () => {
+      previewContainer.style.display = 'flex';
+      let name = myproduct.getAttribute('data-name');
+      previewBox.forEach(preview => {
+        let target = preview.getAttribute('data-target');
+        if (name == target) {
+          preview.classList.add('active');
+        }
+      });
+    };
+  });
+
+
+  previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+      close.classList.remove('active');
+      previewContainer.style.display = 'none';
+    };
+  });
+}
