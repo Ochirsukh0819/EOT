143197236b639ed92f0202727ecf3d5f2fae9970
Temuulen918 <112910669+Temuulen918@users.noreply.github.com>
2023-05-04 09:30:16 +0800

    pop up aldaa zasav

--- javascript/pop_up.js
+++ javascript/pop_up.js
@@ -2,10 +2,10 @@ window.onload = function () {
   const previewContainer = document.querySelector('.products-preview');
   let previewBox = previewContainer.querySelectorAll('.preview');
 
-  document.querySelectorAll('#food .myproduct').forEach(myproduct => {
+  document.querySelectorAll('#food .myproduct a').forEach(myproduct => {
     myproduct.onclick = () => {
       previewContainer.style.display = 'flex';
-      let name = myproduct.getAttribute('data-name');
+      let name = myproduct.parentElement.getAttribute('data-name');
       previewBox.forEach(preview => {
         let target = preview.getAttribute('data-target');
         if (name == target) {
